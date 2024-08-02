c65aa476a5f478e5c19679924c447bcef4b8910c
alxmart <alxmartinez@gmail.com>
2024-08-02 12:57:58 -0300

    Tests using Int,Double,Bool,Character,String,Interpolation, typealias

--- Aula01.playground/Contents.swift
+++ Aula01.playground/Contents.swift
@@ -1,3 +1,55 @@
-import UIKit
+/*
+//var nome = "Zé Ruela"
+var nome : String = "Zé Ruela"
+//var idade = 99
+var idade : Int = 99
+print(nome)
+print(idade)
+print("Meu nome é \(nome) e a minha idade é \(idade)")
+//idade = Int(22.5)
+print(idade)
+//let pi = 3.14
+//print(pi)
+*/
 
-var greeting = "Hello, playground"
+var nome : String = "Zé Ruela"
+var idade : Int = 99
+
+print("Meu nome é \(nome)")
+print(idade)
+
+var coeficienteRendimento : Double = 9.5
+var possuiHistoricoReprovacao: Bool
+possuiHistoricoReprovacao = false
+
+/*
+ if (possuiHistoricoReprovacao == false) {
+ print("Não possui reprovação")
+ } else {
+ print("Existe reprovação")
+ }
+ */
+var turnoEmQueEstuda : Character = "D"
+
+typealias Estudante = String
+
+var aluno1: Estudante = "João"
+var aluno2: Estudante = "Ana"
+
+var inicialDoNome : Character = "A"
+var name = "Alexandre"
+var apelido = "Alex"
+var age = 255
+var altura = 2.14
+var gostaDeSwift = true
+
+print()
+print("Inicial do nome: \(inicialDoNome)")
+print("Nome: \(name)")
+print("Apelido: \(apelido)")
+print("Idade: \(age)")
+print("Altura: \(altura)")
+print("Gosta de Swift: \(gostaDeSwift)")
+
+
+
